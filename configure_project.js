500fb156a930b1faf1993cd6017f34d46a31f36a
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-05-30 18:01:38 +0100

    Re-designing the template

--- configure_project.js
+++ configure_project.js
@@ -1,10 +1,11 @@
 'use strict'
 
-const fs = require('fs');
+const fs   = require('node:fs');
 const path = require('path');
 
 const cwd = process.cwd();
 const { stdin, stdout } = process;
+
 
 /**
  * Taken directly from https://stackoverflow.com/questions/17837147/user-input-in-node-js
@@ -12,14 +13,15 @@ const { stdin, stdout } = process;
  */
 function prompt(question)
 {
-	return new Promise((resolve, reject) =>
-	{
-		stdin.resume();
-		stdout.write(question);
+    return new Promise((resolve, reject) =>
 
-		stdin.on('data', data => resolve(data.toString().trim()));
-		stdin.on('error', err => reject(err));
-  	});
+    {
+        stdout.write(question);
+        stdin.resume();
+
+        stdin.on('data', data => resolve(data.toString().trim()));
+        stdin.on('error', err => reject(err));
+    });
 }
 
 
@@ -28,21 +30,29 @@ function prompt(question)
  * 
  * @param {*} dirname 
  * @param {[fs.Dirent]} files 
+ * @param {[fs.Dirent]} dirs 
  */
-function readDir(dirname, files)
+function readDir(dirname, files, dirs)
 {
     fs.readdirSync(dirname, { withFileTypes: true }).forEach( item =>
     {
         // If it's part of .git/ or build/, ignore it
         if (item.name == ".git") return;
-        if (item.name == "build") return;
-        if (item.name == "configure_project.js") return;
+        else if (item.name == "build") return;
+        else if (item.name == "configure_project.js") return;
+        else if (item.name == "node_modules") return;
+        else if (item.name == "package-lock.json") return;
+        else if (item.name == "package.json") return;
         
         // Get the full name
         item.name = path.join(dirname, item.name);
         
-        // If it's a directory, investigate it
-        if (item.isDirectory()) readDir(item.name, files);
+        // If it's a directory, investigate (and log) it
+        if (item.isDirectory())
+        {
+            dirs.push(item);
+            readDir(item.name, files, dirs);
+        }
 
         // If it's a file, add it to the list of files
         if (item.isFile()) files.push(item);
@@ -51,40 +61,82 @@ function readDir(dirname, files)
 
 async function main()
 {
-    // Read the current working directory
-	/**
-	 * @type {[fs.Dirent]}
-	 */
+    /**
+     * @type {[fs.Dirent]}
+     */
     var files = [];
-    readDir(cwd, files);
+    /**
+     * @type {[fs.Dirent]}
+     */
+    var dirs  = [];
+    // Read the current working directory
+    readDir(cwd, files, dirs);
 
     // Ask the user what they want as their project name to be
-	/**
-	 * @type {string}
-	 */
-	const projName = await prompt("What's the project name? ");
-	const projID = Math.floor((Math.random() * 899999) + 100000);
+    /**
+     * @type {string}
+     */
+    const projName = await prompt("What's the project name? ");
+    const execName = await prompt("What's the executable name? ");
+    const projID   = Math.floor((Math.random() * 899999) + 100000);
 
-	if (projName.includes(' ') || projName.includes('<') || projName.includes('>'))
-	{
-		console.error("Forbidden project name");
-		process.exit(1);
-	}
+    if (projName.includes(' ') || projName.includes('<') || projName.includes('>'))
+    {
+        console.error("Forbidden project name");
+        process.exit(1);
+    }
 
-	// Write this data to all of the files
-	files.forEach(file =>
-	{
-		var f = fs.readFileSync(file.name, 'utf-8');
+    // Setup the patterns
+    const patterns =
+    [
+        { pattern: /__PROJID__/g,     val: projID   },
+        { pattern: /<PROJECT_NAME>/g, val: projName },
+        { pattern: /<EXEC>/g,         val: execName }
+    ];
 
-		f = f.replace(/__PROJID__/g, projID);
-		f = f.replace(/<PROJECT_NAME>/g, projName);
+    // Rename any dirs that match the patterns
+    dirs.forEach(dir => 
+    {
+        patterns.forEach(pat => {
+            // Check if this dir matches the pattern
+            if (pat.pattern.test(dir.name))
+            {
+                var dirNew = dir;
+                dirNew.name.replace(pat.pattern, pat.val);
 
-		fs.writeFileSync(file.name, f);
-	});
+                // fs.renameSync(dir, dirNew);
+            }
+        });
+    });
 
-	console.log(`Configured projectID: ${projID} with name ${projName}!`);
+    // Apply changes to directories in memory
+    files.forEach(file =>
+    {
+        patterns.forEach(pat =>
+        {
+            if (pat.pattern.test(file.name)) file.name.replace(pat.pattern, pat.val);
+        });
+    });
+    
 
-	process.exit();
+    // Write this data to all of the files
+    files.forEach(file =>
+    {
+        console.log(file);
+
+        var f = fs.readFileSync(file.name, 'utf-8');
+
+        patterns.forEach(pat => {
+            f = f.replace(pat.pattern, pat.val);
+        });
+        
+        // fs.writeFileSync(file.name, f);
+    });
+
+    console.log(`Configured projectID: ${projID} with name ${projName}!`);
+    console.log(`The hello world executable is named "${execName}"`);
+
+    process.exit();
 }
 
 main();
