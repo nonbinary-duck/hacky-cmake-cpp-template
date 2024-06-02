5ebfff9672366d037e3759834841996be4f0fdf4
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-06-02 21:42:44 +0100

    Created ignore rules

--- init_project.cpp
+++ init_project.cpp
@@ -1,5 +1,8 @@
 #include <filesystem>
+#include <exception>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <random>
 #include <regex>
 
@@ -30,7 +33,7 @@ struct ValidKeyValidator : public CLI::Validator
             if (!(0 < str.length() && 60 >= str.length())) return std::string("Key must be greater than zero characters and ≤ 60 long");
             
             // Check if the first character is a number
-            if (0x61 <= str[0] && 0x7a >= str[0]) return std::string("First character of key must be [a-z]");
+            if (!(0x61 <= str[0] && 0x7a >= str[0])) return std::string("First character of key must be [a-z]");
             
             // Check if every character of the string is a number/letter/underscore or dash
             for (auto &&c : str)
@@ -64,28 +67,29 @@ int main(int argc, char *argv[])
         ->required()
         ->ignore_case();
 
+
     std::string execName = "default";
     app.add_option("exec_name", execName, "The name of the hello world default executable")
         ->check(ValidKey)
         ->required()
         ->ignore_case();
 
-    ulong projNum  = -1;
+
+    ulong projNum        = -1;
     app.add_option("-n,--num", projNum, "Project number, assigned ND random 6-digit number by default")
         ->check(CLI::Range(0ul,999'999ul))
         ->ignore_case();
     
-    bool skipInput = false;
-    app.add_flag("-y,--no-prompt", skipInput, "Use to accept changes to CWD without prompt")
-        ->ignore_case();
 
-    bool dryRun    = false;
+    bool dryRun          = false;
     app.add_flag("-d,--dry-run", dryRun, "Perform a dry-run execution without making changes")
         ->ignore_case();
 
-    bool selfDestruct = true;
-    app.add_flag("!-q,!--self-destruct", selfDestruct, "Do not remove this binary and its source after operation")
-        ->ignore_case()->default_val(true);
+
+    bool selfDestruct    = true;
+    app.add_flag("!-q,!--no-self-destruct", selfDestruct, "Do not remove this binary and its source after operation")
+        ->ignore_case()
+        ->default_val(true);
 
 
     // Init app
@@ -106,13 +110,71 @@ int main(int argc, char *argv[])
         projNumStr[i] = std::to_string(( (ulong)( projNum / std::pow(10, 5-i) ) ) % 10)[0];
     }
     
-
-    // By this point we have all infos
-    // Now go through CWD and move directories or files that match pattern
+    // Get the current working directory
     auto cwd = std::filesystem::current_path();
 
-    // Quickly ask for manual input
+    // Check if there is a gitignore and .git directory (check we're in the right place)
+    if (!std::filesystem::exists(cwd / ".git") | !std::filesystem::exists(cwd / ".gitignore"))
+        throw std::runtime_error("Must be executed where CWD is a directory with git is set up and a gitignore file");
 
+
+    // Read entries in the gitignore and treat each line as a regex rule
+    std::ifstream gitignoreStream( cwd / ".gitignore" , std::ios::binary);
+    // Start with some additional rules
+    std::vector<std::regex> ignoreRules = {
+        std::regex(".git"),
+        std::regex(".vscode"),
+        std::regex(".gitignore"),
+        std::regex("CLI11.hpp")
+    };
+
+
+    // It really should always open the file stream, since we know the file exists
+    if (!gitignoreStream.is_open()) throw std::runtime_error("Failed to open .gitignore file");
+    else
+    {
+        // Read the gitignore line-by-line
+        for (std::string line; std::getline(gitignoreStream, line);)
+        {
+            // Ignore "rule" if the line is empty or if its a comment
+            if      (line.empty())   continue;
+            else if (line[0] == '#') continue;
+
+            // Add this line as a rule
+            ignoreRules.push_back( std::regex(line) );
+        }
+        
+    }
+    
+
+    ///
+    // By this point we know what action to take
+    //
+    
+    
+
+    // Go through CWD and move directories or files that match pattern
+    for (auto &&path : std::filesystem::recursive_directory_iterator(cwd))
+    {
+        bool skipThisPath = false;
+
+        // if (((std::string)path.path()).find() )
+        // Loop over all ignore rules and ignore if it matches
+        for (auto &&rule : ignoreRules)
+        {
+            // Check if we ignore this path
+            if (std::regex_search(path.path().c_str(), rule))
+            {
+                skipThisPath = true;
+                break;
+            }
+        }
+
+        // Apply the ignore rules
+        if (skipThisPath) continue;
+        
+        printf("%s\n", path.path().c_str());
+    }
     
 
     // Summarise action
